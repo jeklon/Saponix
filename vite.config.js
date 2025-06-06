c0bf4dbd1c6b31e61f76972d0916fb5a4d6cec51
jeklon <jeklon3000@gmail.com>
2025-06-06 22:39:49 +0300

    back to v1

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "./",
-});
+  base: '/Saponix/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
