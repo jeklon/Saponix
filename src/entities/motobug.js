a189c9b057f7ff6280344512ca1cfb5490835d8b
jeklon <jeklon3000@gmail.com>
2025-06-06 22:16:56 +0300

    Initial commit

---
+++ src/entities/motobug.js
@@ -0,0 +1,25 @@
+import k from "../kaplayCtx";
+
+export function makeMotobug(pos) {
+  return k.add([
+    k.sprite("motobug", { anim: "run" }),
+    k.area({ shape: new k.Rect(k.vec2(-5, 0), 32, 32) }),
+    k.scale(4),
+    k.anchor("center"),
+    k.pos(pos),
+    k.offscreen(),
+    "enemy",
+  ]);
+}
+
+export function makeMotobugPasha(pos) {
+  return k.add([
+    k.sprite("motobugPasha", { anim: "run" }),
+    k.area({ shape: new k.Rect(k.vec2(-5, 0), 32, 32) }),
+    k.scale(4),
+    k.anchor("center"),
+    k.pos(pos),
+    k.offscreen(),
+    "enemy",
+  ]);
+}
